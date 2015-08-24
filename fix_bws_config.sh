75bfd2b25ce362803a5a6c2a94caa3151cdf6af9
locutus <ryan@molecke.com>
2015-08-24 12:36:02 -0600

    initial files test

---
+++ fix_bws_config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i "s!uri.*,!uri:\ '$MONGOLAB_URI',!" node_modules/bitcore-wallet-service/config.js
+sed -i "s!port:\ 3232,!port:\ $PORT,!" node_modules/bitcore-wallet-service/config.js
