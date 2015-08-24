7b6a1e883366bbe69803594385625b04baf5099d
locutus <ryan@molecke.com>
2015-08-24 13:08:01 -0600

    up

--- fix_bws_config.sh
+++ fix_bws_config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed -i "s!uri.*,!uri:\ '$MONGOLAB_URI',!" node_modules/bitcore-wallet-service/config.js
 sed -i "s!port:\ 3232,!port:\ $PORT,!" node_modules/bitcore-wallet-service/config.js
+sed -i "s!basePath:\ '\/bws\/api',!basePath:\ '\/',!" node_modules/bitcore-wallet-service/config.js
